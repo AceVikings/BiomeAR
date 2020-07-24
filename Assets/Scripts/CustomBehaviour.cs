87e8bfc5b2f42a3c67cd94bfb9c4848bf830246e
akashjha011 <akash.jsmith@gmail.com>
2020-07-24 23:31:58 +0530

    added echoAR

---
+++ Assets/Scripts/CustomBehaviour.cs
@@ -0,0 +1,86 @@
+﻿/**************************************************************************
+* Copyright (C) echoAR, Inc. 2018-2020.                                   *
+* echoAR, Inc. proprietary and confidential.                              *
+*                                                                         *
+* Use subject to the terms of the Terms of Service available at           *
+* https://www.echoar.xyz/terms, or another agreement                      *
+* between echoAR, Inc. and you, your company or other organization.       *
+***************************************************************************/
+using System.Collections;
+using System.Collections.Generic;
+using UniRx.Triggers;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CustomBehaviour : MonoBehaviour
+{
+    [HideInInspector]
+    public Entry entry;
+
+    private GameObject sphere;
+    private string URL;
+    /// <summary>
+    /// EXAMPLE BEHAVIOUR
+    /// Queries the database and names the object based on the result.
+    /// </summary>
+
+    // Use this for initialization
+    void Start()
+    {
+        sphere = StaticPrefabHolder.Sphere;
+        // Add RemoteTransformations script to object and set its entry
+        gameObject.AddComponent<RemoteTransformations>().entry = entry;
+        
+        // Qurey additional data to get the name
+        string value = "";
+        if (entry.getAdditionalData() != null && entry.getAdditionalData().TryGetValue("name", out value))
+        {
+            // Set name
+            gameObject.name = value;
+        }
+        if (entry.getAdditionalData() != null && entry.getAdditionalData().TryGetValue("URL", out value))
+        {
+            URL = value;
+        }
+        if (entry.getAdditionalData() != null && entry.getAdditionalData().TryGetValue("type", out value))
+        {
+            if (value != "video")
+            {
+                gameObject.AddComponent<Floating>();
+            }
+            else
+            {
+                gameObject.AddComponent<ExpFaceCamera>();
+            }
+        }
+        
+
+    }
+
+    // void GoToURL()
+    // {
+    //     Application.OpenURL(URL);
+    // }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        // RaycastHit Hit;
+        //  
+        // if (Input.GetMouseButtonDown(0))
+        // {
+        //     Debug.Log("Click");
+        //     Debug.Log(Physics.Raycast(ray, out Hit));
+        //     Debug.Log(Hit.collider.gameObject.name);
+        //     Debug.Log(gameObject.name);
+        //     if (Physics.Raycast(ray, out Hit) && Hit.collider.gameObject == gameObject)
+        //     {
+        //         Debug.Log("HIT");
+        //         GoToURL();
+        //     }
+        // }   
+    }
+    
+    
+}
---
+++ Assets/Scripts/Floating.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Floating : MonoBehaviour
+{
+    public float horiSpeed;
+    public float vertiSpeed;
+    public float amplitude;
+
+    public Vector3 tempPoisition;
+
+    private void Start()
+    {
+        
+        vertiSpeed = 0.3f;
+        amplitude = 0.5f;
+        tempPoisition = transform.position;
+    }
+
+    void FixedUpdate()
+    {
+        transform.position = new Vector3(transform.position.x,Mathf.Sin(Time.realtimeSinceStartup*vertiSpeed)*amplitude-1,transform.position.z);
+        // tempPoisition.y =Mathf.Sin(Time.realtimeSinceStartup*vertiSpeed)*amplitude-1;
+        // transform.position = tempPoisition;
+    }
+}
